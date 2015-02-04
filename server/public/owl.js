564fbe4fd6127d32d27dc80096c9f1dfe48d0770
瘦的销魂 <tut.bear@gmail.com>
2015-02-04 12:06:56 +0800

    修改guid的串特征

--- server/public/owl.js
+++ server/public/owl.js
@@ -172,7 +172,7 @@ var OWL = {
 	 * 生成唯一标识码
 	 */
 	getGuid : function(){
-	    return 'xxxxxxxx-xxxx-4xxx-yxxx-xxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
+	    return 'xxxxxxxxxxxx4xxxyxxxxxxxxxxxxxxx'.replace(/[xy]/g, function(c) {
 	        var r = Math.random()*16|0, v = c == 'x' ? r : (r&0x3|0x8);
 	        return v.toString(16);
 	    });
